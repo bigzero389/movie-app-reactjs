40d9ec82300f2c048e0ade43b178817cd19bbfc1
bigzero <garack@gmail.com>
2021-04-20 15:36:27 +0900

    #2.3

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from "react";
 
+
+const foodILike = [
+  {
+    id: 1,
+    name : "kimchi",
+    image : "https://www.koreanbapsang.com/wp-content/uploads/2019/11/DSC_0831.jpg"
+  },
+  {
+    id: 2,
+    name: "kimbap",
+    image : "https://www.koreanbapsang.com/wp-content/uploads/2018/09/DSC_0427-e1613706244682.jpg"
+  },
+  {
+    id: 3,
+    name: "bulgogi",
+    image : "https://www.koreanbapsang.com/wp-content/uploads/2010/05/DSC_0893-e1426734907897.jpg"
+  },
+  {
+    id: 4,
+    name: "bibimbap",
+    image : "https://www.koreanbapsang.com/wp-content/uploads/2010/02/DSC_0417-e1421376856367.jpg"
+  }
+];
+
 function Food({name, picture}) {
   return (
    <div>
      <h2>I like {name} </h2>
-     <img src= {picture} />
+     <img src= {picture} width="50" height="50" alt={name}/>
    </div> 
   );
 }
 
-const foodILike = [
-  {
-    name : "kimchi",
-    image : "'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/220px-Various_kimchi.jp"
-  },
-  {
-    name: "kimbap",
-    image : "https://en.wikipedia.org/wiki/Gimbap#/media/File:Vegetable_gimbap.jpg"
-  }
-];
-
 function App() {
   return (
     <div>
-      {foodILike.map(m => <Food name={m.name} picture={m.image} />)}
+      {
+        foodILike.map(dish => (
+          <Food key={dish.id} name={dish.name} picture={dish.image} />
+        ))
+      }
     </div>
   );
 }
