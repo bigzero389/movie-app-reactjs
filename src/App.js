688e90bce41be6ba21d22dbcb685a2bec4267451
bigzero <garack@gmail.com>
2021-04-21 15:59:14 +0900

    #3.2

--- src/App.js
+++ src/App.js
@@ -45,6 +45,12 @@ import React from "react";
 // }
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  };
+
   state = {
     count: 0
   };
@@ -58,7 +64,20 @@ class App extends React.Component {
     this.setState(current => ({count: current.count - 1}));
   });
 
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("bye");
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div> 
         <h1>The number is : {this.state.count}</h1>
